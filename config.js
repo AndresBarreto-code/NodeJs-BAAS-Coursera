76bd38541f662a7b54c038ca6ad8149ccb8c4216
Andrés Felipe Barreto Guio <64596541+AndresBarreto-code@users.noreply.github.com>
2020-11-08 20:11:06 -0500

    secret

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '751891245655052',
-        clientSecret: '328799ed44c4a9cea2a5036ae248a2e0'
+        clientId: 'secret',
+        clientSecret: 'secret'
     }
 }
